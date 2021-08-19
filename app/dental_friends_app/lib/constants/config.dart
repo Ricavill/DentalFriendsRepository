e173a87815122a581f9281b7c8f4be9a17245355
Ricavill <56647079+Ricavill@users.noreply.github.com>
2021-08-19 18:52:59 -0500

    Revert "Update config.dart"
    This reverts commit 252f5cd82b511af547ebf539d7fbcbf8b55a8b4e.

--- app/dental_friends_app/lib/constants/config.dart
+++ app/dental_friends_app/lib/constants/config.dart
@@ -1,5 +1,5 @@
 //PRODUCCION
-//String apiUrl = 'http://dental';
-//String apiUrl = 'http://67.205.165.99';
+//String apiUrl = 'http://dentalfriends.ec:80';
+//String apiUrl = 'http://67.205.165.126';
 //DESARROLLO
 String apiUrl = 'http://192.168.100.61:3000';
